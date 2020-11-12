e79d170f527171944ca8a72859fd6cb70485f17a
Colter Ulrich <cju5025@gmail.com>
2020-11-11 21:36:32 -0700

    Added favorites feature

---
+++ lib/models/User.rb
@@ -0,0 +1,20 @@
+class User < ActiveRecord::Base
+    has_many :favorites
+    has_many :jokes, through: :favorites
+
+    def find_favorite_ids
+        joke_ids = self.favorites.map {|favorite| favorite.joke_id}
+    end
+
+    def find_favorite_jokes
+        find_favorite_ids
+            joke_ids.map do |id|
+            Joke.find_by id: id
+        end
+    end
+
+    def display_favorite_jokes
+        puts find_favorite_jokes.map {|joke| TTY::Box.frame(width: 90, height: 11, border: :thick) {joke.content}}.uniq
+    end
+
+end
